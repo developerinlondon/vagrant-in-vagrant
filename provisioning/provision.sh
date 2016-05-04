01c87da9c3933522e38dd3b5d0ff401f5f9924e8
nayeem syed <developerinlondon@gmail.com>
2016-05-04 14:31:25 +0300

    Vagrant optimizations

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -3,5 +3,8 @@ sudo apt-get update -qq
 sudo apt-get install ansible -qqyy
 
 echo "Runing ansible scripts"
-
-sudo ansible-playbook /vagrant/provisioning/playbook.yml --inventory-file=/vagrant/provisioning/inventory --tags=local --connection=local
+sudo ansible-playbook /vagrant/provisioning/playbook.yml --inventory-file=/vagrant/provisioning/inventory --tags=osupgrade --connection=local
+sudo ansible-playbook /vagrant/provisioning/playbook.yml --inventory-file=/vagrant/provisioning/inventory --tags=lxc --connection=local
+sudo ansible-playbook /vagrant/provisioning/playbook.yml --inventory-file=/vagrant/provisioning/inventory --tags=vagrant --connection=local
+echo "Executing bagrant up"
+sudo runuser -l root -c 'cd /vagrant/vagrant-host/ && vagrant up'
