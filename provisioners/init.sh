d5c7ed64fc4e17d27436c13bd44a5cc335d9a1a1
Nayeem Syed <developerinlondon@gmail.com>
2016-05-05 10:18:18 +0100

    tidy up

--- provisioners/init.sh
+++ provisioners/init.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-apt-get install -y vagrant
+apt-get install -y vagrant git
 apt install lxc -y
 apt-get update
 apt-get install -y zlib1g-dev
 vagrant plugin install vagrant-lxc
-cd /vagrant/vagrant-host
+cd /vagrant/vagrant-in-vagrant
 vagrant up
---
+++ provisioners/init_lxc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+apt-get install -y git
+apt-get update
