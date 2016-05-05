049fc6daa013f092a9637ed9120667c991680df4
Nayeem Syed <developerinlondon@gmail.com>
2016-05-05 12:25:54 +0100

    removed vagrant box add

--- provisioners/init.sh
+++ provisioners/init.sh
@@ -8,6 +8,4 @@ apt-get update
 apt-get install -y zlib1g-dev
 vagrant plugin install vagrant-lxc
 cd /vagrant/vagrant-in-vagrant
-vagrant box add developerinlondon/ubuntu_lxc_wily_x64
-vagrant box add developerinlondon/ubuntu_lxc_xenial_x64
 vagrant up
