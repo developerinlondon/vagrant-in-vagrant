561f14cdf822b833a3ba4b2bdd51c9734060c55e
Nayeem Syed <developerinlondon@gmail.com>
2016-05-05 12:15:20 +0100

    pre-downloading the vm boxes

--- provisioners/init.sh
+++ provisioners/init.sh
@@ -8,4 +8,6 @@ apt-get update
 apt-get install -y zlib1g-dev
 vagrant plugin install vagrant-lxc
 cd /vagrant/vagrant-in-vagrant
+vagrant box add developerinlondon/ubuntu_lxc_wily_x64
+vagrant box add developerinlondon/ubuntu_lxc_xenial_x64
 vagrant up
