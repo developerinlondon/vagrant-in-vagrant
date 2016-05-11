3c3f98ee8aaff08ce0fc9d1243cf380390815b4a
Nayeem Syed <developerinlondon@gmail.com>
2016-05-11 13:02:38 +0100

    install lxc 2

--- provisioners/init_lxc.sh
+++ provisioners/init_lxc.sh
@@ -2,5 +2,11 @@
 
 set -ex
 
-apt-get install -y git htop aptitude grub-pc grub2
+apt-get install -y git htop aptitude grub-pc grub2 dbus ufw ntp
 apt-get update
+
+# install lxc 2
+apt-add-repository ppa:ubuntu-lxc/stable -y
+apt update
+apt dist-upgrade
+apt install -y lxd
